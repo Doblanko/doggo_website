e64be0296397bb29942e79f4f32d94b7aa92c118
Tyler  Doblanko <tylerdoblanko@gmail.com>
2022-02-26 09:49:19 -0700

    added code formatting

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config' // make this the top import so anything that uses the env variables is defined
+import 'dotenv/config'; // make this the top import so anything that uses the env variables is defined
 import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
@@ -12,38 +12,44 @@ app.use(express.json()); // allow to extract data payload from POST request body
 app.use(express.urlencoded({ extended: true })); // allow to extract data payload from post request from HTML form
 
 // for authentication
-app.use(session({ secret: process.env.session_secret, resave: false, saveUninitialized: true }));
+app.use(
+  session({
+    secret: process.env.session_secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(
-    new LocalStrategy((username, password, done) => {
-        username.findOne( { username: username}, (err, user) => {
-            if (err) {
-                return done(err);
-            }
+  new LocalStrategy((username, password, done) => {
+    username.findOne({ username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
 
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username'});
-            }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
 
-            if (user.password !== password) {
-                return done(null, false, { message: 'Incorrect Password'})
-            }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect Password' });
+      }
 
-            return done(null, user);
-        });
-    })
+      return done(null, user);
+    });
+  })
 );
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    });
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000!')
-})
+  console.log('Listening on port 3000!');
+});
